d044a290e6cdf6c66b2d4b412d539142b47a879c
HasanAmmar05 <altalib.hasan05@gmail.com>
2025-02-25 11:38:44 +0800

    Add Arcjet middleware for rate limiting and bot detection; update dependencies and subscription routes

--- app.js
+++ app.js
@@ -8,16 +8,22 @@ import { connect } from 'mongoose';
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
 import cookieParser from 'cookie-parser';
+import arcjetMiddleware from './middlewares/arcjet.middleware.js';
+import dotenv from 'dotenv';
+dotenv.config({ path: `.env${process.env.NODE_ENV || 'development'}.local` });
+
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded( { extended: false}));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 app.use(`/api/v1/auth`, authRouter);
 app.use(`/api/v1/users`, userRouter);
 app.use(`/api/v1/subscriptions`, subscriptionRouter);
+app.set('trust proxy', true); // Trust the first proxy
 
 app.use(errorMiddleware);
 
--- config/env.js
+++ config/env.js
@@ -4,5 +4,7 @@ config({ path: `.env${process.env.NODE_ENV || 'development'}.local` });
 export const { PORT = 5500, 
     NODE_ENV = 'development',
     DB_URI = 'mongodb+srv://Hasan05:Hf8a9vir3cZ9azz3@cluster0.ejzhu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    JWT_SECRET = "secret", JWT_EXPIRES_IN = "1d"
+    JWT_SECRET = "secret", JWT_EXPIRES_IN = "1d",
+    ARCJET_KEY = "ajkey_01jmxfracwfzft9j82p8krtha7",    
+    ARCJET_ENV = "development"
  } = process.env;
